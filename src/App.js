14d45c023ba30b77a4aab6844b6f9927b5ad88d1
Francis Bulus <notchera@gmail.com>
2019-10-01 07:14:00 +0100

    added search functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { TodoForm } from "./components/TodoComponents/TodoForm";
 import TodoList from "./components/TodoComponents/TodoList";
 import "./Todo.css";
+import Search from './components/TodoComponents/Search'
 
 class App extends React.Component {
   state = {
@@ -25,7 +26,7 @@ class App extends React.Component {
   };
 
   addTask = task => {
-    if (localStorage.getItem("todos").length < 1) {
+    if (!localStorage.getItem("todos")) {
       const todos = [task];
       localStorage.setItem("todos", JSON.stringify(todos));
     } else {
@@ -52,15 +53,30 @@ class App extends React.Component {
     });
   };
 
+  handleSearch = event => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    const searchQuery = event.target.value.toLowerCase();
+    const displayedTodos = storedTodos.filter(el => {
+      const searchValue = el.title.toLowerCase();
+      return searchValue.indexOf(searchQuery) !== -1;
+    });
+
+    this.setState({
+      todos: displayedTodos
+    });
+  };
+ 
+
   render() {
     return (
       <div className="container">
         <div className="todo-app">
           <div className="left">
             <h1 className="header">Get Stuff Done!</h1>
+            <Search handleSearch={this.handleSearch}/>
           </div>
           <div className="right">
-            <TodoForm addTask={this.addTask} className="form"/>
+            <TodoForm addTask={this.addTask} className="form" />
             <TodoList
               todos={this.state.todos}
               deleteTask={this.deleteTask}
